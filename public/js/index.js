2d58528a00279fd34fffc4f7f22b81dc56f1e228
Hammad Shaikh <hammadkhs@gmail.com>
2018-10-28 11:36:57 -0400

    Successfully take in problem and process corresponding input using the POST method instead of websocket connections

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,44 @@
 $(document).ready(function () {
     "use strict";
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
+    // window.WebSocket = window.WebSocket || window.MozWebSocket;
+    //
+    // let connection = connectToServer();
+    //
+    // connection.onopen = function () {
+    //     console.log('Connection with server established.');
+    //     //connection.send("I have connected to you.");
+    // };
+    //
+    // connection.onclose = function () {
+    //     console.log('Disconnected from server.');
+    //
+    // };
+    //
+    // connection.onmessage = function (data) {
+    //     console.log(data.data);
+    // };
 
-    let connection = connectToServer();
-
-    connection.onopen = function () {
-        console.log('Connection with server established.');
-        //connection.send("I have connected to you.");
-    };
-
-    connection.onclose = function () {
-        console.log('Disconnected from server.');
-        
-    };
-
-    connection.onmessage = function (data) {
-        console.log(data.data);
-    };
-
-    $('#form').on('submit', function (e) {
-        e.preventDefault();
-        let problem = $("input[name='problem'][type='radio']:checked").val();
-        let data, file, fr;
-        if (problem === "Monte Carlo")
-            data = $("#montecarlo-input").val();
-        else {
-           data = $("#mergesort-input");
-           }
-        //console.log(problem + ": " + data);
-        if(problem.length > 0) {
-            let jsonObj = {
-                name: problem.toString(),
-                input: data
-            };
-            let str = JSON.stringify(jsonObj);
-            //console.log(str);
-            connection.send(str);
-            console.log("Problem Emitted.", str);
-        }
-    });
+    // $('#form').on('submit', function (e) {
+    //     e.preventDefault();
+    //     let problem = $("input[name='problem'][type='radio']:checked").val();
+    //     let data, file, fr;
+    //     if (problem === "Monte Carlo")
+    //         data = $("#montecarlo-input").val();
+    //     else {
+    //        data = $("#mergesort-input");
+    //        }
+    //     //console.log(problem + ": " + data);
+    //     if(problem.length > 0) {
+    //         let jsonObj = {
+    //             name: problem.toString(),
+    //             input: data
+    //         };
+    //         let str = JSON.stringify(jsonObj);
+    //         //console.log(str);
+    //         connection.send(str);
+    //         console.log("Problem Emitted.", str);
+    //     }
+    // });
     $('#montecarlo').on('click', function() {
         $('#montecarlo-input').removeAttr('disabled');
         $('#mergesort-input').attr('disabled', true);
@@ -50,7 +50,7 @@ $(document).ready(function () {
         $('#btn-submit').removeAttr('disabled');
     });
     
-    function connectToServer() {
-        return new WebSocket('ws://localhost:3000', 'echo-protocol');
-    }
+    // function connectToServer() {
+    //     return new WebSocket('ws://localhost:3000', 'echo-protocol');
+    // }
 });
--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '/public');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 let app = express();
-app.use(express.static(publicPath));
-let WebSocketServer = require('websocket').server;
-let http = require('http');
+
+const WebSocketServer = require('websocket').server;
+const http = require('http');
 
 let server = http.createServer(app);
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+
+app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+function convertToInt(item, index) {
+    return parseInt(item);
+}
+
+app.post('/problem', function (req, res) {
+    let prob = req.body.problem;
+    if (prob === "Merge Sort") {
+        let file = req.files.array;
+        let arr = file.data.toString().split(',');
+        let numArr = arr.map(Number);
+        console.log(`problem: ${prob}`);
+        console.log(numArr);
+    } else {
+        console.log(req.body)
+    }
+    res.send(`<h1>${prob} Submitted</h1>`);
+});
 
 server.listen(port, function() {
     console.log((new Date()) + ` Server is listening on port ${port}`);
@@ -45,4 +71,6 @@ wsServer.on('request', function(request) {
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
+
+
 });
