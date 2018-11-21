4eb79cabf0d17ec158f9340f6ab64f4abeb57edf
Hammad Shaikh <hammadkhs@gmail.com>
2018-11-20 20:56:22 -0500

    Task database set up. Using MongoDB

--- server.js
+++ server.js
@@ -8,35 +8,62 @@ const WebSocketServer = require('websocket').server;
 const http = require('http');
 
 let server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const bodyParser = require('body-parser');
-
 
 app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-function convertToInt(item, index) {
-    return parseInt(item);
-}
+//MongoDB set up
+const mongoose = require('mongoose');
 
+//mongoose maintains connection with mongodb over time
+mongoose.connect('mongodb://localhost:27017/task_queue', {useNewUrlParser: true});
+
+let Task = mongoose.model('task_queue', {
+    problemType: {
+        type: String
+    },
+    data: {
+        type: String
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+//Once a user submits a task to the server
 app.post('/problem', function (req, res) {
+    //Parse the task to get the problem type and input data
     let prob = req.body.problem;
+    let input;
     if (prob === "Merge Sort") {
         let file = req.files.array;
-        let arr = file.data.toString().split(',');
-        let numArr = arr.map(Number);
+        input = file.data.toString();
         console.log(`problem: ${prob}`);
-        console.log(numArr);
+        console.log(input);
     } else {
-        console.log(req.body)
+        input = req.body.numberOfPoints;
+        console.log(input);
     }
+    let newTask = new Task({
+        problemType: prob,
+        data: input
+    });
+
+    newTask.save().then((document) => {
+        console.log('Task added to queue', document);
+    }, (err) => {
+        console.log('Unable to add task to queue');
+    });
     res.send(`<h1>${prob} Submitted</h1>`);
 });
 
 server.listen(port, function() {
-    console.log((new Date()) + ` Server is listening on port ${port}`);
+    console.log((new Date().toISOString().replace('T', ' ').substr(0, 19)) + ` Server is listening on port ${port}`);
 });
 
 /*WebSocketServer will be used to connect with IOS clients*/
