a8e8a9ca0caa0fb67ea0023d2e6d0db3cd6b69a2
Hammad Shaikh <hammadkhs@gmail.com>
2018-12-14 00:54:26 -0500

    testing

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ function delegate() {
                     return console.log("No Clients Available At The Moment.");
                 } else {
                     for (let i = 0; i < clnts.length; i++) {
-                        clients[clnts[i].listIndex].send(JSON.stringify({type: 'monte carlo', data: '100000'}));
+                        clients[clnts[i].listIndex].send(tasks[0]);
                     }
                 }
             });
