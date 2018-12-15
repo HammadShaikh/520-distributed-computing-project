f819cf03c6b36ac03b33c68c3735def8360cd789
Hammad Shaikh <hammadkhs@gmail.com>
2018-12-15 16:39:23 -0500

    testing:

--- server.js
+++ server.js
@@ -221,13 +221,14 @@ wsServer.on('request', function(request) {
                 Task.findOneAndUpdate({_id: client.probId}, {$inc: {nodes: -1, pointsGenerated: Number(message.utf8Data)}}, {new: true}, (err, doc) => {
                     if(err) {return console.log('error updating', err);}
                     //Task.findOne({_id: client.workingOn})
+
+                    if (doc.nodes === 0) {
+                        let res = (4*doc.pointsGenerated)/doc.data;
+                        Task.updateOne({_id: client.workingOn}, {mcSolution: res, status: 'complete'}, (err, doc) => {
+
+                        });
+                    }
                     console.log('after updating..\n', doc);
-                    // if (doc.nodes === 0) {
-                    //     let res = (4*doc.pointsGenerated)/doc.data;
-                    //     Task.updateOne({_id: client.workingOn}, {mcSolution: res}, (err, doc) => {
-                    //
-                    //     });
-                    // }
                 });
             });
 
