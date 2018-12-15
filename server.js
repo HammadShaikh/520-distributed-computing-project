e46af37759c5b5058387fcd7260810812aecc0cd
Hammad Shaikh <hammadkhs@gmail.com>
2018-12-14 22:10:11 -0500

    testing

--- server.js
+++ server.js
@@ -232,10 +232,7 @@ function delegate() {
                         if (tasks[0].problemType === 'Monte Carlo') {
                             console.log(`Sending ${partition} points to ${clnts[i].ipAddress}`);
                         }
-                        clients[clnts[i].listIndex].send(JSON.stringify({
-                            problemType: 'Monte Carlo',
-                            data: partition
-                        }));
+                        clients[clnts[i].listIndex].send(JSON.stringify(tasks[0]));
                     }
                     Task.updateOne({_id: tasks[0]._id}, {status: 'in progress'}, (err, raw) => {
                         if(err) {}
